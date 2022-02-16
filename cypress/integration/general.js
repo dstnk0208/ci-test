11e47cda32745339b1ac5d73f4ec49b1d9bdecd9
Kohei Hozumi <k.hozumi@thedott.io>
2022-02-16 23:56:17 +0900

    modified url on visit()

--- cypress/integration/general.js
+++ cypress/integration/general.js
@@ -1,9 +1,9 @@
 describe('ci-test general', () => {
     before(() => {
-        cy.visit('http://localhost:4200/')
+        cy.visit('/')
     })
     it('general test', () => {
-        cy.get('button').get('[class="test-button"]').click()
+        cy.get('[class="test-button"]').click()
         cy.get('[class="test-div"')
             .expect(text()).to.eq("Fired")
     })
